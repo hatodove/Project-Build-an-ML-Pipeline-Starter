a699651db9d282a691e264bc56b1dd614668977c
Praise Sutton <psutto10@wgu.edu>
2025-09-19 23:53:40 +0900

    steps wired & running

--- main.py
+++ main.py
@@ -51,9 +51,17 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/get_data",
+                "main",
+                parameters={
+                    "sample": config["etl"]["sample"],
+                    "artifact_name": "sample.csv",
+                    "artifact_type": "raw_data",
+                    "artifact_description": "Raw file as downloaded"
+                },
+            )
+
             pass
 
         if "data_check" in active_steps:
